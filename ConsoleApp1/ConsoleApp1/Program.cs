4ed532e149237811c4b34090d5ed6bb68c747032
LordOfTheBeer <116457854+LordOfTheBeer@users.noreply.github.com>
2023-03-18 13:06:11 +0100

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Punkt p1 = new Punkt(3, 4);
+            Punkt p2 = new Punkt(-1, 5);
+            p1.Wyswietl();
+            p1.przesun(1, 2);
+            p1.Wyswietl();
+            p1.Cofnij_Przesun();
+            p1.Wyswietl();
+            Console.WriteLine(Punkt.Odleglosc2punktow(p1, p2));
+
+
+
+
+
+        }    
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Punkt.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Punkt
+    {
+        private int x;
+
+        private int y;
+
+        private int xp, yp;
+
+        public Punkt(int wsp_x, int wsp_y)
+
+        {
+            x = wsp_x; y = wsp_y;
+        }
+        public void Wyswietl()
+        {
+            Console.WriteLine("x = " + x + " y = " + y ); 
+        }
+        public void przesun(int dod_x, int dod_y)
+        {
+            x = x + dod_x;
+            y = y + dod_y;
+            xp = dod_x;
+            yp = dod_y;
+            
+        }
+       public void Cofnij_Przesun()
+        {
+            x = x - xp;
+            y = y - yp;
+        }
+        public static double Odleglosc2punktow(Punkt p1, Punkt p2)
+        {
+           return Math.Sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+        }
+       
+    }
+        
+        
+        
+}
